341e427977d05ec8c3628d4e7c0860fc7e5c8aa2
Letícia Lopes <leticialopesrv@gmail.com>
2024-11-21 19:34:30 -0300

    comecei a validar o nome
    falta muita coisa aaaaa

---
+++ scripts/validarForm.js
@@ -0,0 +1,18 @@
+const formPet = document.getElementById('formPet');
+
+const inputs = document.querySelectorAll('.input-dados');
+
+const spans = document.querySelectorAll('.spanRequired');
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+
+function nameValidate(){
+    if(){
+        console.log('O Nome não pode ser nulo');
+
+    } else{
+        console.log('O nome foi preenchido');
+    }
+}
