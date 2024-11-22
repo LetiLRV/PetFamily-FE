961207abe2cbd63fbef2f89c4c00df71a71e8017
Letícia Lopes <leticialopesrv@gmail.com>
2024-11-21 21:35:54 -0300

    aaaaaa desisto

--- scripts/validarForm.js
+++ scripts/validarForm.js
@@ -1,18 +1,13 @@
 const formPet = document.getElementById('formPet');
 
-const inputs = document.querySelectorAll('.input-dados');
+function nameValidate(){    
+    const name = document.getElementById('name');
 
-const spans = document.querySelectorAll('.spanRequired');
-
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-
-
-function nameValidate(){
-    if(){
-        console.log('O Nome não pode ser nulo');
+    if(name.value == ""){
+        console.log("O Nome não pode ser nulo");
+        alert("coloca a poha de um nome");
 
     } else{
-        console.log('O nome foi preenchido');
+        console.log("O nome foi preenchido");
     }
 }
