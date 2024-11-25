11596028151bde7465f15b235447c625215466f8
Letícia Lopes <leticialopesrv@gmail.com>
2024-11-25 15:20:46 -0300

    att

--- scripts/validarForm.js
+++ scripts/validarForm.js
@@ -1,10 +1,8 @@
-
 function enviarForm(e){
     e.preventDefault()
 
-    const nomePet = (document.getElementById('nomePet')).value
+    const nomePet = (document.getElementById('nomePet')).value;
 
-    console.log(typeof (nomePet));
 
     if(typeof nomePet != "string" || nomePet == null || nomePet == ""){
         console.log("Nome do pet está vazio ou incorreto");
